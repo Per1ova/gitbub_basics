d0928ab694574eaf7a4fc3ed6e4766af34bbbb3f
Per1ova <azzabimourad2@gmail.com>
2025-02-10 11:22:00 +0100

    flybyObjects add new nemes

--- var.dart
+++ var.dart
@@ -1,7 +1,15 @@
 var name = 'Voyager I';
 var year = 1977;
 var antennaDiameter = 3.7;
-var flybyObjects = ['Jupiter', 'Saturn', 'Uranus', 'Neptune'];
+var flybyObjects = [
+  'Jupiter',
+  'Saturn',
+  'Uranus',
+  'Neptune',
+  'Earth',
+  'Mars',
+  'Neptune'
+];
 var image = {
   'tags': ['saturn'],
   'url': '//path/to/saturn.jpg'
